53574cda2848f0c75e85f6e0c2187e9499b71b02
Maximilian Muecke <mmuecke@MININT-00HIUC0.localdomain>
2022-05-30 22:16:15 +0200

    added java docker hello world

---
+++ java-docker-hello-world/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.print("Java program Hello world From Docker");
+  }
+}
---
+++ java-docker-hello-world/README.md
@@ -0,0 +1,6 @@
+You can run the following to build and run the docker image:
+
+```bash
+docker build -t java-demo
+docker run -it --rm java-demo
+```
